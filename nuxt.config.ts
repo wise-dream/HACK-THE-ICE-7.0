2a77813b3b318baf1d593c9bd7d829367c45378a
wise-dream <kamilvinokurov@gmail.com>
2025-11-15 13:39:26 +0500

    Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt'
+  ]
+})
